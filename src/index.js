4952e2199435df8888a1ee4ce13bf50f16443da6
ElenaKilljoy <lenokcardio@gmail.com>
2023-01-25 16:25:21 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = false;
+  let resultStr = str;
+  let i = 0;
+  let x = 1;
+  let y = 0;
+    while (y < bracketsConfig.length) {
+      while (x < resultStr.length) {
+        if (resultStr[i] + resultStr[x] === bracketsConfig[y][0] + bracketsConfig[y][1]) {
+          resultStr = resultStr.slice(0, i) + resultStr.slice(x + 1, resultStr.length);
+          result = result || true;
+        }
+      i = i + 1;
+      x = x + 1;
+      result = result || false;
+      }
+    i = 0;
+    x = 1;
+    y = y + 1;
+    }
+  y = 0;
+  if (resultStr === '' && result === true) {
+    return true;
+  } else if (result === false) {
+    return false;
+  } else if (result === true && resultStr !== '') {
+      while (result === true && resultStr !== '') {
+        result = false;
+      while (y < bracketsConfig.length) {
+        while (x < resultStr.length) {
+          if (resultStr[i] + resultStr[x] === bracketsConfig[y][0] + bracketsConfig[y][1]) {
+            resultStr = resultStr.slice(0, i) + resultStr.slice(x + 1, resultStr.length);
+            result = result || true;
+          }
+        i = i + 1;
+        x = x + 1;
+        result = result || false;
+        }
+      i = 0;
+      x = 1;
+      y = y + 1;
+      }
+    y = 0;
+    }
+    if (resultStr === '' && result === true) {
+      return true;
+    } else if (result === false) {
+      return false;
+    }
+    }
 }
